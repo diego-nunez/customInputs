a8275a441e001759b603bcd62f52bb02fc08c54a
rocioOrtiz <61204147+rocioOrtiz@users.noreply.github.com>
2020-11-05 19:02:53 -0600

    Custom inputs
    Custom inputs

---
+++ local/js/ejemplo.js
@@ -0,0 +1,11 @@
+var input = new customizeInputs({
+    exclude: 'noSelected',
+    general: 'customizeInput',
+    floatLabel: {
+        'select': true,
+        'general': true,
+        'excludeClasses': ['noFloatLabel']
+    }
+})
+
+input.customInputs();
